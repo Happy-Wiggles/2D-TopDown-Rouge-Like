267951412f665377d5bded5bad6a3f1aa88e5bee
Justin Scheel <scheel@th-brandenburg.de>
2019-10-22 12:37:50 +0200

    Added Room and RoomController

---
+++ 2D TopDown Rouge-Like/Assets/Room.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Room : MonoBehaviour
+{
+    public string name;
+    public int X;
+    public int Y;
+
+    public int Width = 19;
+    public int Height = 19;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+                
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ 2D TopDown Rouge-Like/Assets/RoomController.cs
@@ -0,0 +1,121 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class RoomInfo{
+    public string name;
+    public int X;
+    public int Y;
+}
+
+public class RoomController : MonoBehaviour
+{
+    RoomController instance;
+
+    List<Room> Rooms = new List<Room>();
+
+    Queue<RoomInfo> loadRoomQueue = new Queue<RoomInfo>();
+    
+    List<Room> loadedRooms = new List<Room>();
+
+    RoomInfo currentLoadRoomInfo = new RoomInfo();
+
+    bool isLoadingRoom;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    void loadRoom(string name, int x, int y)
+    {
+        if (RoomExists(x, y))
+        {
+            return;
+        }
+
+        RoomInfo roomInfo = new RoomInfo();
+        roomInfo.name = name;
+        roomInfo.X = x;
+        roomInfo.Y = y;
+
+        loadRoomQueue.Enqueue(roomInfo);
+        
+    }
+
+    IEnumerator loadRoomRoutine(RoomInfo roomInfo)
+    {
+        string currentRoomName = roomInfo.name;
+
+        AsyncOperation loadRoom = SceneManager.LoadSceneAsync(currentRoomName.ToString(), LoadSceneMode.Additive);
+
+        while (!loadRoom.isDone)
+        {
+            yield return null;
+        }
+    }
+
+    public void RegisterRoom(Room room)
+    {
+        room.transform.position = new Vector3(
+            currentLoadRoomInfo.X * room.Width, 
+            currentLoadRoomInfo.Y * room.Height,
+            0);
+
+        room.X = currentLoadRoomInfo.X;
+        room.Y = currentLoadRoomInfo.Y;
+        room.name = currentLoadRoomInfo.name;
+
+        room.transform.parent = transform;
+
+        isLoadingRoom = false;
+
+        loadedRooms.Add(room);
+    }
+
+    bool RoomExists(int x, int y)
+    {
+        foreach (Room room in loadedRooms)
+        {
+            if (room.X == x && room.Y == y) 
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        loadRoom("Start", 0, 0);
+        loadRoom("Empty", 1, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        UpdateRoomQueue();
+    }
+
+    void UpdateRoomQueue()
+    {
+        if (isLoadingRoom)
+        {
+            return;
+        }
+
+        if (loadRoomQueue.Count == 0)
+        {
+            return;
+        }
+
+        currentLoadRoomInfo = loadRoomQueue.Dequeue();
+        isLoadingRoom = true;
+
+        StartCoroutine(loadRoomRoutine(currentLoadRoomInfo));
+
+    }
+}
