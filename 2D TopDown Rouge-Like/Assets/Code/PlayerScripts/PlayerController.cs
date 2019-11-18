a731472025b2462c598104e0bb796709fdb032b3
Justin Scheel <scheel@th-brandenburg.de>
2019-11-18 11:38:18 +0100

    Added basic animation

--- 2D TopDown Rouge-Like/Assets/Code/PlayerScripts/PlayerController.cs
+++ 2D TopDown Rouge-Like/Assets/Code/PlayerScripts/PlayerController.cs
@@ -5,11 +5,13 @@ using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour
 {
-
-    public float Speed;
+    public Animator animator;
+    public float Speed = 5;
     Rigidbody2D RigidBody;
     public Text CollectedText;
     public static int CollectedAmount = 0;
+    private bool isMoving = false;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -18,12 +20,23 @@ public class PlayerController : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
         
         RigidBody.velocity = new Vector3(horizontal * Speed, vertical * Speed, 0);
         CollectedText.text = "Number of collected items: " + CollectedAmount;
+
+        if (!(RigidBody.velocity.x == 0 && RigidBody.velocity.y == 0))
+        {
+            isMoving = true;
+        }
+        else
+        {
+            isMoving = false;
+        }
+        
+        animator.SetBool("IsMoving", isMoving);
     }
 }
